a40c6082c489138326d85a23d8d94732b5540329
Aaron Snowberger <jekkilekki@gmail.com>
2019-12-12 11:34:07 +0900

    [1.12] Change post_meta, remove author, add categories, add reading-time

--- functions.php
+++ functions.php
@@ -183,3 +183,91 @@ function edxchild_register_colophon_menu() {
 	register_nav_menu( 'colophon', __( 'Colophon Menu', 'edxchild' ) );
 }
 add_action( 'after_setup_theme', 'edxchild_register_colophon_menu' );
+
+/**
+ * Function to calculate and return post reading time.
+ */
+function edxchild_reading_time() {
+
+	$content = get_post_field( 'post_content', get_the_ID() );
+	$count   = str_word_count( wp_strip_all_tags( $content ) );
+	$time    = $count / 250; // Roughly 250 wpm reading time.
+
+	$min = floor( $time % 60 ); // Get only the min (left of the decimal).
+	$sec = $time - (int) $time; // Get only the sec (right of the decimal).
+	$sec = round( $sec * 60 );  // Convert decimal min (0.296) to seconds.
+	$sec = $sec < 10 ? ( '0' . $sec ) : $sec; // If seconds are less than 10, pad with a front 0.
+
+	return $min . ':' . $sec;
+}
+
+/**
+ * Function to add reading time to post meta.
+ */
+function edxchild_add_reading_time() {
+
+	/**
+	 * Static variables allow exist only in a local function scope,
+	 * BUT also do NOT lose their value when the program execution leaves this scope.
+	 *
+	 * So, we can store data here that will be called and remembered - perfect for post_meta.
+	 * We could use $calls, $call++, $calls = 0 - but I'll use a string to mimic the TwentyTwenty function.
+	 *
+	 * @link https://www.php.net/manual/en/language.variables.scope.php#language.variables.scope.static .
+	 */
+	static $meta_location = 'single-top';
+
+	if ( 'single-top' === $meta_location ) {
+		?>
+
+	<li class="post-reading-time meta-wrapper">
+		<span class="meta-icon">
+			<span class="screen-reader-text"><?php esc_html_e( 'Reading Time', 'edxchild' ); ?></span>
+			<i class="far fa-clock"></i>
+		</span>
+		<span class="meta-text">
+			<?php printf( __( '%s Reading time', 'edxchild' ), edxchild_reading_time() ); // phpcs:ignore ?>
+		</span>
+	</li>
+
+		<?php
+		$meta_location = 'single-bottom';
+	} else {
+		$meta_location = 'single-top';
+	}
+
+}
+add_action( 'twentytwenty_start_of_post_meta_list', 'edxchild_add_reading_time', 10, 1 );
+
+/**
+ * Function to remove author from top post meta.
+ */
+function edxchild_remove_author_post_meta() {
+
+	// Remove 'author' from the post_meta array.
+	return array(
+		'post-date',
+		'comments',
+		'sticky',
+	);
+
+}
+add_filter( 'twentytwenty_post_meta_location_single_top', 'edxchild_remove_author_post_meta' );
+
+/**
+ * Function to add categories and comments to bottom post meta.
+ *
+ * @param array $post_meta The $post_meta array.
+ */
+function edxchild_add_bottom_post_meta( $post_meta ) {
+
+	// Add 'categories' to bottom post meta.
+	$add_meta = array(
+		'categories',
+	);
+
+	// Merge with current $post_meta.
+	return array_merge( $post_meta, $add_meta );
+
+}
+add_filter( 'twentytwenty_post_meta_location_single_bottom', 'edxchild_add_bottom_post_meta' );
